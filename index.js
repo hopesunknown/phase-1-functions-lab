741b311bea9d1f27e95fca23a2a58c92115ba6ba
Jennifer Tchai <jentchai@gmail.com>
2021-12-14 21:35:00 -0500

    commit tree a4f4ce4eeba524d3c43fddceff36fe21ba531ca1

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return (Math.abs(someValue - 42));
+}
+
+function distanceFromHqInFeet(someValue) {
+    return (distanceFromHqInBlocks(someValue) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return ((Math.abs(destination - start)) * 264);
+  }
+
+  function calculatesFarePrice(start, destination) {
+    if (((Math.abs(destination - start)) * 264) < 400) {
+        return 0;
+    } else if (((Math.abs(destination - start)) * 264) < 2000) {
+        return (0.02 * (((Math.abs(destination - start)) * 264) - 400));
+    } else if (((Math.abs(destination - start)) * 264) < 2500 && (((Math.abs(destination - start)) * 264) > 2000)) {
+        return 25;
+    } else (((Math.abs(destination - start)) * 264) > 2500)
+        {
+        return "cannot travel that far";
+    }
+  }
